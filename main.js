cfd54d18dae6517f808c8361e1450e792f1b2184
TheC <i@thec.me>
2018-02-23 17:02:07 +0800

    每次观看视频时顺便获取此视频相关的「猜你喜欢」

--- main.js
+++ main.js
@@ -8,22 +8,62 @@ const DB = {
 	},
 	set(obj, cb) {
 		DB.local.set(obj, cb)
+	},
+	getViewHistory() {
+		let data = window.localStorage.getItem('biliHistoryData');
+		if( !data ) {
+			return [];
+		}
+		return JSON.parse(data);
+	}
+}
+
+// ajax
+const HTTP = {
+	get(url) {
+		return new Promise((resolve, reject) => {
+			let xhr = new XMLHttpRequest();
+			xhr.open('GET', url);
+			xhr.onload = () => {
+				if( xhr.status == 200 ) {
+					resolve(xhr.responseText);
+				} else {
+					reject(xhr.status);
+				}
+			}
+			xhr.send();
+		});
 	}
 }
 
 // 获取推荐
 const RECOMMAND = {
-	// 从服务端获取推荐视频数据
+	// 从服务端获取当前视频页的推荐视频
 	update() {
-
-	},
-	// 从全部尚未观看的推荐视频里随机选出total个
-	get(total) {
-
-	},
-	// 初始化推荐数据库，如果当前数据库不为空则不做任何事，如果为空就根据用户访问历史去获取推荐视频
-	init() {
-		
+		let url = window.location.href,
+				m = /\/av(\d+)\//.exec(url);
+		if( !m ) {
+			return console.error(`找不到av号：${url}`);
+		}
+		let av = m[1];
+		HTTP.get(`https://comment.bilibili.com/recommendnew,${av}`).then((raw) => {
+			let res;
+			try {
+				res = JSON.parse(raw);
+			} catch(e) {
+				return console.error(`解析recommandnew接口返回值失败：${e}`);
+			}
+			let data = res.data;
+			// 已推荐次数、本次操作执行的时间戳
+			data.forEach((v) => {
+				v._count = 0,
+				v._ts = new Date();
+			});
+			// 保存到数据库
+			let d = {};
+			d[av] = data;
+			DB.set(d);
+		});
 	}
 }
 
@@ -55,7 +95,7 @@ const UI = {
 
 // 当前是否首页？
 if( UI.isIndex() ) {
-	RECOMMAND.init();
+	RECOMMAND.update();
 	UI.insertRecommand();
 }
 // 当前是否视频播放页？
