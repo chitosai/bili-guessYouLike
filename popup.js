df7e7876cee8b13d0335a39fb420839e177fcc1a
Shu Niu <niu541412@gmail.com>
2018-09-06 02:28:15 +0800

    判断为Firefox时，更换API
    Firefox缺少 getBytesInUse 这个API， 采用获取所有keys计算长度的方法

--- popup.js
+++ popup.js
@@ -9,16 +9,32 @@ function init() {
 		document.querySelector('#videosCount').textContent = data['count'] || 0;
 	});
 
-	// 数据大小
-	chrome.storage.local.getBytesInUse(null, (bytes) => {
-		let d = 0;
-		if( bytes < 1048576 ) {
-			d = ( (bytes / 1024).toFixed(1) + 'KB');
-		} else {
-			d = ( (bytes / 1048576).toFixed(1) + 'MB');
-		}
-		document.querySelector('#storageUsage').textContent = d;
-	});
+  // 判断是否为firefox
+  var isFirefox = typeof InstallTrigger !== 'undefined';
+
+  // 数据大小
+  if(isFirefox) {
+    chrome.storage.local.get(null, (items) => {
+      bytes = JSON.stringify(items).length;
+      let d = 0;
+      if (bytes < 1048576) {
+        d = ((bytes / 1024).toFixed(1) + 'KB');
+      } else {
+        d = ((bytes / 1048576).toFixed(1) + 'MB');
+      }
+      document.querySelector('#storageUsage').textContent = d;
+    });
+  } else {
+    chrome.storage.local.getBytesInUse(null, (bytes) => {
+      let d = 0;
+      if( bytes < 1048576 ) {
+        d = ( (bytes / 1024).toFixed(1) + 'KB');
+      } else {
+        d = ( (bytes / 1048576).toFixed(1) + 'MB');
+      }
+      document.querySelector('#storageUsage').textContent = d;
+    });
+  }
 }
 
 init();
